45e6bd1c19fa11ec6548f68bfee4a16f172547cd
primitive0 <94762195+primitive0@users.noreply.github.com>
2024-11-10 20:32:00 +0300

    Добавил тест для utf8::next_char

--- src/utf8_test.cpp
+++ src/utf8_test.cpp
@@ -3,21 +3,32 @@
 #include "types/string.hpp"
 #include "utf8.hpp"
 
-TEST_CASE("hello, world") {
-    String string{"foo"};
+TEST_CASE("utf8::next_char works") {
+    // $ - 1 byte, ñ - 2, 漢 - 3, 𝄞 - 4 in UTF-8
+    String string = "$ñ漢𝄞";
 
     auto cursor = string.cbegin();
     auto end = string.cend();
 
     auto result = utf8::next_char(cursor, end);
     REQUIRE(result.has_value());
-    REQUIRE(*result == U'f');
+    REQUIRE(*result == U'$');
 
     result = utf8::next_char(cursor, end);
     REQUIRE(result.has_value());
-    REQUIRE(*result == U'o');
+    REQUIRE(*result == U'ñ');
 
     result = utf8::next_char(cursor, end);
     REQUIRE(result.has_value());
-    REQUIRE(*result == U'o');
+    REQUIRE(*result == U'漢');
+
+    result = utf8::next_char(cursor, end);
+    REQUIRE(result.has_value());
+    REQUIRE(*result == U'𝄞');
+
+    result = utf8::next_char(cursor, end);
+    REQUIRE(!result.has_value());
+    REQUIRE(result.error() == utf8::DecodingError::Eof);
 }
+
+// todo: add more tests
